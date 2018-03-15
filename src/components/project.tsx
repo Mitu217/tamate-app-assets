557d38e0ea5297136b469dcda15bb8941864bf6c
Mitu217 <mtksk.26884um@gmail.com>
2018-03-15 10:28:49 +0900

    Create project page

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -12,6 +12,8 @@ import {withStyles} from 'material-ui/styles';
 import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import GridList, { GridListTile, GridListTileBar } from 'material-ui/GridList';
 import Typography from 'material-ui/Typography';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
 
 
 interface Props {
@@ -37,6 +39,35 @@ const styles = theme => ({
         width: `calc(100% - 16px)`,
         overflowY: 'scroll' as 'scroll',
     },
+
+    header: {
+        width: '100%',
+        height: '200px',
+        overflowY: 'hidden' as 'hidden',
+    },
+    headerThumbnail: {
+        backgroundImage: `url('https://www.aniplexplus.com/res/g5b92h?w=510&h=510')`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '180px',
+        width: '180px',
+        margin: '10px',
+        position: 'absolute' as 'absolute',
+        top: '64px',
+        zIndex: 100,
+        boxShadow: '2px 2px 4px gray',
+    },
+    headerBackgroundImage: {
+        backgroundImage: `url('https://www.aniplexplus.com/res/g5b92h?w=510&h=510')`,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '100%',
+        filter: 'blur(10px)',
+        margin: '-10px',  // blurの値と同じ値だけマイナス
+    },
 });
 
 export class Project extends React.Component<Props, {}> {
@@ -47,7 +78,7 @@ export class Project extends React.Component<Props, {}> {
 
     render() {
         const classes = this.props.classes
-        const tileData = [
+        const projectData = [
             {
                 key: 0,
             },
@@ -67,9 +98,39 @@ export class Project extends React.Component<Props, {}> {
                 key: 5,
             },
         ]
+        if (this.props.match.params.id) {
+            const projectId = this.props.match.id
+            const project = projectData[projectId]
+            return (
+                <div>
+                    <div className={classes['header']}>
+                        <div className={classes['headerThumbnail']}/>
+                        <div className={classes['headerBackgroundImage']}/>
+                    </div>
+                    <AppBar position="static" color="default">
+                        <Tabs
+                            value={1}
+                            indicatorColor="primary"
+                            textColor="primary"
+                            scrollable
+                            scrollButtons="auto"
+                        >
+                            <Tab label="Item One" />
+                            <Tab label="Item Two" />
+                            <Tab label="Item Three" />
+                            <Tab label="Item Four" />
+                            <Tab label="Item Five" />
+                            <Tab label="Item Six" />
+                            <Tab label="Item Seven" />
+                        </Tabs>
+                    </AppBar>
+                </div>
+            )
+        }
+
         return (
             <GridList cols={3} cellHeight={'auto'} className={classes['gridList']} spacing={0}>
-                {tileData.map(tile => (
+                {projectData.map(tile => (
                     <GridListTile className={classes['card']} key={tile.key} onClick={() => this.handleLocationChangePath('/projects/' + tile.key)}>
                         <Card className={classes['cardInner']}>
                             <CardMedia
