539b0f140d3cd0cb523dc15893c9f165446019c7
Mitu217 <mtksk.26884um@gmail.com>
2018-03-25 22:28:38 +0900

    Fix Store

--- src/store.ts
+++ src/store.ts
@@ -2,26 +2,29 @@ import {createStore as reduxCreateStore, combineReducers, GenericStoreEnhancer, 
 import tab, {State as TabState, Actions as TabActions} from 'modules/tab';
 import drawer, {State as DrawerState, Actions as DrawerActions} from 'modules/menu-drawer';
 import project, {State as ProjectState, Actions as ProjectActions} from 'modules/project';
-import row, {State as RowState, Actions as RowActions} from 'modules/row';
+import datasource, {State as DatasourceState, Actions as DatasourceActions} from 'modules/datasource';
 import schema, {State as SchemaState, Actions as SchemaActions} from 'modules/schema';
+import config, {State as ConfigState, Actions as ConfigActions} from 'modules/config';
 
 export type ReduxState = {
     tab: TabState
     drawer: DrawerState
     project: ProjectState
-    row: RowState
+    datasource: DatasourceState
     schema: SchemaState
+    config: ConfigState
 }
   
-export type ReduxAction = TabActions | DrawerActions | ProjectActions | SchemaActions | Action
+export type ReduxAction = TabActions | DrawerActions | ProjectActions | SchemaActions | DatasourceActions | ConfigActions | Action
 
 export function createStore(middleware: GenericStoreEnhancer) {
     const reducer = combineReducers({
         tab,
         drawer,
         project,
-        row,
+        datasource,
         schema,
+        config,
     })
     return reduxCreateStore(reducer, middleware)
 }
