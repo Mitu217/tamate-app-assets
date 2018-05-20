4cab98457aa11ae608c4a1bced7590f1eedd5655
Mitu217 <mtksk.26884um@gmail.com>
2018-05-20 23:30:01 +0900

    Add ActivityList at Dashboard

--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,54 +1,73 @@
 <template>
-    <div id="dashboard">
-        <!-- Project List -->
-        <el-card class="project-card">
-            <div slot="header" class="project-card-header">
-                <span>Project</span>
-                <el-button style="float: right; padding: 3px 0" type="text" @click="onClickNewProject()">New Project</el-button>
+    <el-row class="dashboard-content">
+        <el-col class="activity-content" v-loading="activity.loading">
+            <div class="activity-card-content">
+                <el-table :data="activity.items" :show-header="false">
+                    <el-table-column prop="message"></el-table-column>
+                </el-table>
             </div>
-            <el-menu
-                v-loading="project.loading"
-                element-loading-text="Loading..."
-                element-loading-spinner="el-icon-loading"
-                class="project-card-content">
-                <div v-for="project in project.items" :key="project.id" class="text item">
-                    <el-menu-item :index="project.id" @click="onClickProject(project.id)">
-                        <i class="el-icon-goods"></i>
-                        <span>{{project.name}}</span>
-                    </el-menu-item>
+        </el-col>
+        <el-col class="project-content">
+            <el-card class="project-card">
+                <div slot="header" class="project-card-header">
+                    <span>Project</span>
+                    <el-button style="float: right; padding: 3px 0" type="text" @click="onClickNewProject()">New Project</el-button>
                 </div>
-            </el-menu>
-        </el-card>
-    </div>
+                <el-menu
+                    v-loading="project.loading"
+                    element-loading-text="Loading..."
+                    element-loading-spinner="el-icon-loading"
+                    class="project-card-content">
+                    <div v-for="project in project.items" :key="project.id" class="text item">
+                        <el-menu-item :index="project.id" @click="onClickProject(project.id)">
+                            <i class="el-icon-goods"></i>
+                            <span>{{project.name}}</span>
+                        </el-menu-item>
+                    </div>
+                </el-menu>
+            </el-card>
+        </el-col>
+    </el-row>
 </template>
 
 <style>
+.activity-card-content {
+  margin: 20px;
+}
 .activity-card {
-  flex: 1;
-  margin: 8px;
+  padding: 18px 20px;
+  max-width: 480px;
+  margin: 0 auto;
 }
-.project-card-header {
-}
+
 .project-card-content {
   height: 100%;
 }
 .project-card {
   display: flex;
   flex-direction: column;
-  width: 240px;
+
   height: 360px;
   margin: 8px;
 }
-.el-card__header {
-}
+
 .el-card__body {
   padding: 0;
   flex: 1;
 }
-#dashboard {
+
+.activity-content {
+  flex: 1;
+}
+.project-content {
+  width: 260px;
+}
+
+.dashboard-content {
   display: flex;
   flex-direction: row;
   max-width: 960px;
+  height: 100%;
   margin: 0 auto;
 }
 </style>
@@ -59,6 +78,20 @@ export default {
   data() {
     return {
       activity: {
+        items: [
+          {
+            id: "",
+            message: "adminがtamateを作成しました"
+          },
+          {
+            id: "",
+            message: "これはLatestActivityのモックです"
+          },
+          {
+            id: "",
+            message: "ここにはすべてのログが時系列順に表示されます"
+          }
+        ],
         loading: false
       },
       project: {
