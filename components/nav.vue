daf2b4d85964fd0783af7ed53ff5422104c49eef
Mitu217 <mtksk.26884um@gmail.com>
2018-05-21 00:35:23 +0900

    Delete ProjectSetting

--- components/nav.vue
+++ components/nav.vue
@@ -1,35 +1,41 @@
 <template>
-    <div>
-        <el-menu class="el-menu-vertical-demo" :collapse="isCollapse">
-            <!--
-            <el-menu-item index="10" @click="onClickMenuItem('')">
-                <i class="el-icon-menu"></i>
-                <span slot="title">Overview</span>
+    <el-menu :collapse="isCollapse">
+        <!--
+        <el-menu-item index="10" @click="onClickMenuItem('')">
+            <i class="el-icon-menu"></i>
+            <span slot="title">Overview</span>
+        </el-menu-item>
+        -->
+        <el-menu-item index="20" @click="onClickMenuItem('datasources')">
+            <i class="el-icon-goods"></i>
+            <span slot="title">Datasources</span>
+        </el-menu-item>
+        <el-menu-item index="30" @click="onClickMenuItem('schemas')">
+            <i class="el-icon-tickets"></i>
+            <span slot="title">Schemas</span>
+        </el-menu-item>
+        <el-menu-item index="40" @click="onClickMenuItem('rows')">
+            <i class="el-icon-news"></i>
+            <span slot="title">Rows</span>
+        </el-menu-item>
+        <el-menu-item index="50" @click="onClickMenuItem('compare')">
+                <i class="el-icon-sort"></i>
+                <span slot="title">Compare</span>
             </el-menu-item>
-            -->
-            <el-menu-item index="20" @click="onClickMenuItem('datasources')">
-                <i class="el-icon-goods"></i>
-                <span slot="title">Datasources</span>
-            </el-menu-item>
-            <el-menu-item index="30" @click="onClickMenuItem('schemas')">
-                <i class="el-icon-tickets"></i>
-                <span slot="title">Schemas</span>
-            </el-menu-item>
-            <el-menu-item index="40" @click="onClickMenuItem('rows')">
-                <i class="el-icon-news"></i>
-                <span slot="title">Rows</span>
-            </el-menu-item>
-            <el-menu-item index="50" @click="onClickMenuItem('compare')">
-                    <i class="el-icon-sort"></i>
-                    <span slot="title">Compare</span>
-                </el-menu-item>
-            <el-menu-item index="60" @click="onClickMenuItem('setting')">
-                <i class="el-icon-setting"></i>
-                <span slot="title">Setting</span>
-            </el-menu-item>
-        </el-menu>
-    </div>
+        <!--
+        <el-menu-item index="60" @click="onClickMenuItem('setting')">
+            <i class="el-icon-setting"></i>
+            <span slot="title">Setting</span>
+        </el-menu-item>
+        -->
+    </el-menu>
 </template>
+
+<style scoped>
+.el-menu {
+  height: 100%;
+}
+</style>
 
 <script>
 export default {
