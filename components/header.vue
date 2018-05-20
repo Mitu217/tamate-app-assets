7b30fb41d4972953e0dfde73fdac2321cba5106e
Mitu217 <mtksk.26884um@gmail.com>
2018-05-21 00:35:03 +0900

    Set shadow and z-index at Header

--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,14 @@
 <template>
-    <el-menu class="el-menu" mode="horizontal">
+    <el-menu class="el-menu header" mode="horizontal">
         <el-menu-item index="1" @click="onClickTitle()">{{ title }}</el-menu-item>
     </el-menu>
 </template>
 
-<style>
+<style scoped>
+.header {
+  z-index: 1000;
+  box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.1);
+}
 </style>
 
 <script>
