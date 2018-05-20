737ae7ff4d25f57ddf0e8e2c86f0cba6ba3f4052
Mitu217 <mtksk.26884um@gmail.com>
2018-05-20 23:41:22 +0900

    Fix DefaultLayout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,37 @@
 <template>
-    <div class="content">
-        <el-container>
-            <el-header>
-                <tamate-header></tamate-header>
-            </el-header>
-            <el-main>
-                <nuxt/>
-            </el-main>
-        </el-container>
-    </div>
+    <el-container class="content" >
+        <el-header>
+            <tamate-header></tamate-header>
+        </el-header>
+        <el-main>
+            <nuxt/>
+        </el-main>
+    </el-container>
 </template>
 
 <style>
+/* color */
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
 .content {
   height: 100vh;
   width: 100vw;
+  min-width: 760px;
+}
+.el-header {
+  padding: 0;
+}
+.el-main {
+  padding: 20px 0;
+  height: 100%;
 }
 body {
   margin: 0;
