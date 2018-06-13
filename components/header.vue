bd80150f05c2cb27f527327f6ea8579f4ac28877
Mitu217 <mtksk.26884um@gmail.com>
2018-06-13 21:34:51 +0900

    Fix Header for signout

--- components/header.vue
+++ components/header.vue
@@ -1,10 +1,31 @@
 <template>
-    <el-menu class="el-menu header" mode="horizontal">
-        <el-menu-item index="1" @click="onClickTitle()">{{ title }}</el-menu-item>
-    </el-menu>
+    <div class="header">
+        <el-menu mode="horizontal">
+            <el-menu-item index="1" @click="onClickTitle">{{ title }}</el-menu-item>
+        </el-menu>
+        <el-dropdown class="avator" trigger="click" v-if="userName != ''" @command="onClickLogout">
+            <span class="el-dropdown-link">
+                {{ userName }}<i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item>Logout</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+    </div>
 </template>
 
 <style scoped>
+.avator {
+  line-height: 60px;
+  position: absolute;
+  right: 20px;
+  top: 0;
+}
+.el-dropdown-link {
+  cursor: pointer;
+  font-size: 14px;
+  color: #909399;
+}
 .header {
   z-index: 100000;
   box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.1);
@@ -12,15 +33,31 @@
 </style>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      title: "tamate"
+      title: "tamate",
+      userName: ""
     };
+  },
+  mounted: function() {
+    axios
+      .get(this.config.host + "/api/user")
+      .then(res => {
+        //if (res.data.name !== "unknown")
+        this.userName = res.data.name;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   },
   methods: {
     onClickTitle: () => {
       location.href = "/dashboard";
+    },
+    onClickLogout: () => {
+      location.href = "/login?logout=true";
     }
   }
 };
--- pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <a :href="this.config.host + '/login'">ログイン</a>
-</template>
-
-<script>
-export default {};
-</script>
